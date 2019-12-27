1958a7594ff9b54fbe7e659d7aa7717c2ff30b24
Aleksey Pugach <45797477+sherman4321@users.noreply.github.com>
2019-12-28 02:45:41 +0300

    Delete Admin.java

--- code/ShopClasses/Participants/Admin.java
+++
@@ -1,8 +0,0 @@
-package com.example.myApp.ShopClasses.Participants;
-
-public class Admin extends ShopParticipant {
-    public Admin(String userName, String password)
-    {
-        super(userName, password);
-    }
-}
